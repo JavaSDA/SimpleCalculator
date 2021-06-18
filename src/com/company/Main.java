5b506235e3257d8c2c22f48472074cc9307d37bb
Uche Lawal <thatboywayne95@gmail.com>
2021-06-18 19:23:35 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Number Declarations
+        int firstNumber;
+        int secondNumber;
+        int answer;
+
+        // Number initialized
+        firstNumber = 5;
+        secondNumber = 7;
+        answer = 0;
+
+        // Mathematical Operator
+        String operator = "-";
+
+        if (operator.equals("+")) {
+            answer = firstNumber + secondNumber;
+        } else if (operator.equals("-")) {
+            answer = firstNumber - secondNumber;
+        } // Assignment
+//        Do the else if statements for the multiplication and division
+
+        System.out.println(answer);
+
+    }
+}
